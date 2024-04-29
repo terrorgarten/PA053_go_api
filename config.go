1c1424f547c443ef6e969a7933b474abd58c1843
terrorgarten <matejkonopik@gmail.com>
2024-04-29 11:12:39 +0200

    added railway setup

--- config.go
+++ config.go
@@ -8,6 +8,7 @@ import (
 type Config struct {
 	RapidAPIKey   string
 	WeatherAPIKey string
+	Port          string
 }
 
 func LoadConfig() (*Config, error) {
@@ -17,5 +18,6 @@ func LoadConfig() (*Config, error) {
 	return &Config{
 		RapidAPIKey:   os.Getenv("X_RAPIDAPI_KEY"),
 		WeatherAPIKey: os.Getenv("WEATHERAPI_KEY"),
+		Port:          os.Getenv("PORT"),
 	}, nil
 }
--- main.go
+++ main.go
@@ -3,29 +3,40 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
+	"log"
 	"os"
 )
 
 func main() {
 
+	config, err := LoadConfig()
+	if err != nil {
+		log.Print("Failed to load configuration")
+		os.Exit(1)
+	}
+	if config.RapidAPIKey == "" || config.WeatherAPIKey == "" {
+		log.Printf("Missing configuration values: RapidAPIKey=%s, WeatherAPIKey=%s", config.RapidAPIKey, config.WeatherAPIKey)
+		os.Exit(1)
+	}
+	if config.Port == "" {
+		config.Port = "16443"
+	}
+
 	app := fiber.New()
-
 	app.Use(logger.New())
+	setupRoutes(app, config)
 
-	setupRoutes(app)
-
-	if err := app.Listen(":8080"); err != nil {
+	if err := app.Listen("0.0.0.0:" + config.Port); err != nil {
 		os.Exit(1)
 	}
 }
 
-// setupRoutes registers all the application routes
-func setupRoutes(app *fiber.App) {
+func setupRoutes(app *fiber.App, config *Config) {
 	app.Get("/hw3/api", func(c *fiber.Ctx) error {
 		if c.Query("queryStockPrice") != "" {
-			return stockPriceHandler(c)
+			return stockPriceHandler(c, config.RapidAPIKey)
 		} else if c.Query("queryAirportTemp") != "" {
-			return airportTempHandler(c)
+			return airportTempHandler(c, config.WeatherAPIKey)
 		} else if c.Query("queryEval") != "" {
 			return evalHandler(c)
 		} else {
